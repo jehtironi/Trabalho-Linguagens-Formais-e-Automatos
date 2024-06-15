201a88004330d03c77232cf32225e372e0280f76
Jeh Tironi <67989696+jehtironi@users.noreply.github.com>
2024-06-15 14:45:32 -0300

    Update SyntaticError.java

--- Código Java/src/Gals/SyntaticError.java
+++ Código Java/src/Gals/SyntaticError.java
@@ -1,14 +1,17 @@
 package Gals;
 
+// Classe que representa um erro sintático
 public class SyntaticError extends AnalysisError
 {
+// Construtor que recebe uma mensagem e uma posição
     public SyntaticError(String msg, int position)
 	 {
-        super(msg, position);
+        super(msg, position); // Chama o construtor da classe pai
     }
 
+// Construtor que recebe apenas uma mensagem
     public SyntaticError(String msg)
     {
-        super(msg);
+        super(msg); // Chama o construtor da classe pai
     }
 }
