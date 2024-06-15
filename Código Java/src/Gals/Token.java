a7f7477963c5e0849d04b6f90034c679ca01ac50
Jeh Tironi <67989696+jehtironi@users.noreply.github.com>
2024-06-15 14:43:28 -0300

    Update Token.java

--- Código Java/src/Gals/Token.java
+++ Código Java/src/Gals/Token.java
@@ -1,33 +1,40 @@
 package Gals;
 
+// Definição da classe Token
 public class Token
 {
+    // Atributos privados da classe
     private int id;
     private String lexeme;
     private int position;
 
+    // Construtor da classe Token
     public Token(int id, String lexeme, int position)
     {
-        this.id = id;
-        this.lexeme = lexeme;
-        this.position = position;
+        this.id = id; // Inicializa o id
+        this.lexeme = lexeme; // Inicializa o lexeme
+        this.position = position; // Inicializa a position
     }
 
+    // Método getter para id
     public final int getId()
     {
         return id;
     }
 
+    // Método getter para lexeme
     public final String getLexeme()
     {
         return lexeme;
     }
 
+    // Método getter para position
     public final int getPosition()
     {
         return position;
     }
 
+    // Método para retornar a representação em string do Token
     public String toString()
     {
         return id+" ( "+lexeme+" ) @ "+position;
